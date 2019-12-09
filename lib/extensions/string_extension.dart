088b0f1b95bb1652f1c9e42462fd1e4a1586f606
Faiyyaz Shaikh <>
2019-12-09 14:17:34 +0530

    screen utils & removed string util

--- lib/extensions/string_extension.dart
+++
@@ -1,6 +0,0 @@
-extension StringExt on String {
-  bool get isValidEmail {
-    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    return emailRegExp.hasMatch(this);
-  }
-}
--- lib/utils/size_helper.dart
+++ lib/utils/size_helper.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
 class SizeHelper {
+
+  static Size _phoneScreenSize(context){
+    return MediaQuery.of(context).size;
+  }
+
   static Size getDimensions(context) {
-    var phoneScreenSize = MediaQuery.of(context).size;
+   var size = _phoneScreenSize(context);
     if (MediaQuery.of(context).orientation == Orientation.portrait) {
-      return Size(phoneScreenSize.width, phoneScreenSize.height);
+      return Size(size.width, size.height);
     } else {
-      return Size(phoneScreenSize.height, phoneScreenSize.width);
+      return Size(size.height, size.width);
     }
   }
+
+  static double width(width,context){
+    var size = getDimensions(context);
+    return width * (size.width * size.aspectRatio);
+  }
+
+  static double height(height,context){
+    var size = getDimensions(context);
+    return height * (size.height * size.aspectRatio);
+  }
+
+  static double sp(fontSize,context){
+    var size = getDimensions(context);
+    return fontSize * (size.width * size.aspectRatio);
+  }
+
 }
