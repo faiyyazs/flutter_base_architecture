4da88545963028556b0f214593ce46e3a518b143
Faiyyaz Shaikh <>
2020-02-15 10:57:47 +0530

    BaseWidget Loading animate

--- lib/ui/base_widget.dart
+++ lib/ui/base_widget.dart
@@ -6,30 +6,58 @@ class BaseWidget<T extends ChangeNotifier> extends StatefulWidget {
   final T viewModel;
   final Widget child;
   final Function(T) onModelReady;
+  Duration duration;
+  final bool animate;
 
   BaseWidget(
       {Key key,
       @required this.builder,
       @required this.viewModel,
       this.child,
-      this.onModelReady})
+      this.onModelReady,
+      this.duration: const Duration(
+        milliseconds: 800,
+      ),
+      this.animate: true})
       : super(key: key);
 
   @override
   _BaseWidget<T> createState() => _BaseWidget<T>();
 }
 
-class _BaseWidget<T extends ChangeNotifier> extends State<BaseWidget<T>> {
+class _BaseWidget<T extends ChangeNotifier> extends State<BaseWidget<T>>
+    with SingleTickerProviderStateMixin {
   T _model;
+  Duration duration;
+  AnimationController _controller;
 
   @override
   void initState() {
+    _controller = AnimationController(vsync: this, duration: duration);
+    if (widget.animate) {
+      _controller.forward(from: 0.0);
+    }
     super.initState();
     _model = widget.viewModel;
+    duration = widget.duration;
 
     if (widget.onModelReady != null) {
       widget.onModelReady(_model);
     }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(Widget oldWidget) {
+    if (widget != oldWidget && widget.animate) {
+      _controller.forward(from: 0.0);
+    }
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
@@ -38,7 +66,16 @@ class _BaseWidget<T extends ChangeNotifier> extends State<BaseWidget<T>> {
       value: _model,
       child: Consumer<T>(
         builder: widget.builder,
-        child: widget.child,
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _controller.value,
+              child: child,
+            );
+          },
+          child: widget.child,
+        ),
       ),
     );
   }
